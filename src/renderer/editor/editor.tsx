4a7c5659e39101ea1f84e9497174ee9f0fdc5357
Julien Moreau-Mathis <moreau-mathis@intechinfo.fr>
2021-12-14 14:38:20 +0100

    chore: bumping Babylon.JS to v5.0.0

--- src/renderer/editor/editor.tsx
+++ src/renderer/editor/editor.tsx
@@ -1126,7 +1126,7 @@ export class Editor {
 
         // Shortcuts
         window.addEventListener("keyup", (ev) => {
-            this.keyboardEventObservable.notifyObservers(new KeyboardInfo(KeyboardEventTypes.KEYUP, ev));
+            this.keyboardEventObservable.notifyObservers(new KeyboardInfo(KeyboardEventTypes.KEYUP, ev as any));
 
             if (this.preview.canvasFocused) {
                 if (ev.key === "t") { return this.preview.setGizmoType(GizmoType.Position); }
@@ -1164,7 +1164,7 @@ export class Editor {
         });
 
         window.addEventListener("keydown", (ev) => {
-            this.keyboardEventObservable.notifyObservers(new KeyboardInfo(KeyboardEventTypes.KEYDOWN, ev));
+            this.keyboardEventObservable.notifyObservers(new KeyboardInfo(KeyboardEventTypes.KEYDOWN, ev as any));
 
             if (ev.ctrlKey && SceneSettings.Camera) {
                 for (const i in SceneSettings.Camera.inputs.attached) {
